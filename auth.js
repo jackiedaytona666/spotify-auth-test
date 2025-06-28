7829c76e1df2e82b83f910dba9912d411c7f1318
jackiedaytona666 <ricks.holmberg@gmail.com>
2025-06-28 02:44:44 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,4 @@
-const link = document.getElementById("auth-link");
-
+// Build the Spotify OAuth authorization URL with required parameters
 const params = new URLSearchParams({
   client_id: "801cfb401a964dfd89360c06f2c7303a",
   response_type: "token",
@@ -7,4 +6,5 @@ const params = new URLSearchParams({
   scope: "user-top-read"
 });
 
+// Assign the generated URL to the auth button's href
 link.href = `https://accounts.spotify.com/authorize?${params.toString()}`;
